8a478de7bea8c3c952ca05e45177fc850b0dfaba
NavjotSingh0748 <navjot212023@gmail.com>
2026-05-06 17:29:50 +0530

    Updated SQL analysis files

--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -29,3 +29,33 @@ From Batting
 Group BY PlayerName 
 ORDER BY TotalFours DESC
 Limit 10;
+
+--Best Strike Rate
+Select PlayerName,
+	Round(avg(StrikeRate)) as StrikeRate
+From Batting
+Group By PlayerName
+Having SUM(Balls)>100
+ORDER BY StrikeRate DESC
+Limit 10;
+
+--Teams with most Runs
+Select Team,
+	Sum(Runs) as TotalRuns
+From Batting
+Group By Team
+Order By TotalRuns Desc;
+
+--Teams with most IN first Innings
+Select Team,
+	Sum(Runs) as TotalRuns
+From Batting
+WHERE InningsNo = 1
+Group By Team
+Order By TotalRuns Desc;
+
+--Comparison of runs scored in first and second innings
+Select InningsNo,
+	Sum(Runs) as TotalRuns
+From Batting
+Group by InningsNo;
